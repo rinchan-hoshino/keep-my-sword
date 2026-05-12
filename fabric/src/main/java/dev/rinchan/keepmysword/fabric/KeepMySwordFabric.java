1908da5f41fe85bd75e1eb0e6f94930d1971d276
rinchanai <275440557+rinchanai@users.noreply.github.com>
2026-05-12 21:27:39 +0800

    Add Minecraft 1.12.2 Forge build

--- fabric/src/main/java/dev/rinchan/keepmysword/fabric/KeepMySwordFabric.java
+++
@@ -1,9 +0,0 @@
-package dev.rinchan.keepmysword.fabric;
-
-import net.fabricmc.api.ModInitializer;
-
-public final class KeepMySwordFabric implements ModInitializer {
-    @Override
-    public void onInitialize() {
-    }
-}
--- forge/src/main/java/dev/rinchan/keepmysword/forge/KeepMySwordForge.java
+++ forge/src/main/java/dev/rinchan/keepmysword/forge/KeepMySwordForge.java
@@ -3,7 +3,7 @@ package dev.rinchan.keepmysword.forge;
 import dev.rinchan.keepmysword.KeepMySword;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(KeepMySword.MOD_ID)
+@Mod(modid = KeepMySword.MOD_ID, name = "Keep My Sword", version = "0.1.0", dependencies = "required-after:rinlib@[0.1.0,)")
 public final class KeepMySwordForge {
     public KeepMySwordForge() {
     }
